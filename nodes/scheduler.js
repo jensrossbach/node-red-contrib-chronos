226f87a3fdcc61a96065d20e6d86fd5a949cbf67
Jens Rossbach <devel@jrossbach.de>
2020-12-27 11:21:23 +0100

    Support for loading schedule events from context variables

--- nodes/scheduler.js
+++ nodes/scheduler.js
@@ -50,14 +50,19 @@ module.exports = function(RED)
             node.status({});
             chronos.init(RED, node.config.latitude, node.config.longitude, node.config.sunPositions);
 
-            node.schedule = settings.schedule;
             node.multiPort = settings.multiPort;
-
             node.disabledSchedule = false;
 
-            node.ports = [];
+            node.schedule = [];
+            for (let i=0; i<settings.schedule.length; ++i)
+            {
+                node.schedule.push({id: i+1, config: settings.schedule[i]});
+            }
+
             if (node.multiPort)
             {
+                node.ports = [];
+
                 for (let i=0; i<settings.outputs; ++i)
                 {
                     node.ports.push(null);
@@ -67,23 +72,11 @@ module.exports = function(RED)
             let valid = true;
             for (let i=0; i<node.schedule.length; ++i)
             {
-                let data = node.schedule[i];
+                let event = node.schedule[i].config;
 
-                // check for valid user time
-                if ((data.trigger.type == "time") && !chronos.isValidUserTime(data.trigger.value))
+                if ((event.trigger.type == "global") || (event.trigger.type == "flow"))
                 {
-                    valid = false;
-                    break;
-                }
-
-                // check for valid output and convert according to type
-                if (data.output.type == "fullMsg")
-                {
-                    try
-                    {
-                        data.output.value = JSON.parse(data.output.value);
-                    }
-                    catch (e)
+                    if (!event.trigger.value)
                     {
                         valid = false;
                         break;
@@ -91,41 +84,19 @@ module.exports = function(RED)
                 }
                 else
                 {
-                    if (!data.output.property.name)
+                    // check for valid user time
+                    if ((event.trigger.type == "time") && !chronos.isValidUserTime(event.trigger.value))
                     {
                         valid = false;
                         break;
                     }
 
-                    if (data.output.property.type == "num")
-                    {
-                        if (+data.output.property.value === +data.output.property.value)
-                        {
-                            data.output.property.value = +data.output.property.value;
-                        }
-                        else
-                        {
-                            valid = false;
-                            break;
-                        }
-                    }
-                    else if (data.output.property.type == "bool")
-                    {
-                        if (/^(true|false)$/.test(data.output.property.value))
-                        {
-                            data.output.property.value = (data.output.property.value == "true");
-                        }
-                        else
-                        {
-                            valid = false;
-                            break;
-                        }
-                    }
-                    else if (data.output.property.type == "json")
+                    // check for valid output and convert according to type
+                    if (event.output.type == "fullMsg")
                     {
                         try
                         {
-                            data.output.property.value = JSON.parse(data.output.property.value);
+                            event.output.value = JSON.parse(event.output.value);
                         }
                         catch (e)
                         {
@@ -133,16 +104,61 @@ module.exports = function(RED)
                             break;
                         }
                     }
-                    else if (data.output.property.type == "bin")
+                    else
                     {
-                        try
-                        {
-                            data.output.property.value = Buffer.from(JSON.parse(data.output.property.value));
-                        }
-                        catch (e)
+                        if (!event.output.property.name)
                         {
                             valid = false;
                             break;
+                        }
+
+                        if (event.output.property.type == "num")
+                        {
+                            if (+event.output.property.value === +event.output.property.value)
+                            {
+                                event.output.property.value = +event.output.property.value;
+                            }
+                            else
+                            {
+                                valid = false;
+                                break;
+                            }
+                        }
+                        else if (event.output.property.type == "bool")
+                        {
+                            if (/^(true|false)$/.test(event.output.property.value))
+                            {
+                                event.output.property.value = (event.output.property.value == "true");
+                            }
+                            else
+                            {
+                                valid = false;
+                                break;
+                            }
+                        }
+                        else if (event.output.property.type == "json")
+                        {
+                            try
+                            {
+                                event.output.property.value = JSON.parse(event.output.property.value);
+                            }
+                            catch (e)
+                            {
+                                valid = false;
+                                break;
+                            }
+                        }
+                        else if (event.output.property.type == "bin")
+                        {
+                            try
+                            {
+                                event.output.property.value = Buffer.from(JSON.parse(event.output.property.value));
+                            }
+                            catch (e)
+                            {
+                                valid = false;
+                                break;
+                            }
                         }
                     }
                 }
@@ -187,8 +203,6 @@ module.exports = function(RED)
 
                     if (typeof msg.payload == "boolean")
                     {
-                        stopTimers();
-
                         if (msg.payload)
                         {
                             startTimers();
@@ -196,6 +210,7 @@ module.exports = function(RED)
                         }
                         else
                         {
+                            stopTimers();
                             node.disabledSchedule = true;
                         }
 
@@ -206,13 +221,13 @@ module.exports = function(RED)
                         let numDisabled = 0;
                         for (let i=0; (i<msg.payload.length) && (i<node.schedule.length); ++i)
                         {
-                            if (msg.payload[i] && !node.schedule[i].timer)
+                            if (msg.payload[i])
                             {
-                                setUpTimer(node.schedule[i], false);
+                                startTimer(node.schedule[i]);
                             }
-                            else if (!msg.payload[i] && node.schedule[i].timer)
+                            else
                             {
-                                tearDownTimer(node.schedule[i]);
+                                stopTimer(node.schedule[i]);
                             }
 
                             if (!msg.payload[i])
@@ -242,7 +257,7 @@ module.exports = function(RED)
 
             node.schedule.forEach(data =>
             {
-                setUpTimer(data, false);
+                startTimer(data);
             });
         }
 
@@ -252,43 +267,78 @@ module.exports = function(RED)
 
             node.schedule.forEach(data =>
             {
-                if ("timer" in data)
-                {
-                    tearDownTimer(data);
-                }
+                stopTimer(data);
             });
+        }
+
+        function startTimer(data)
+        {
+            stopTimer(data);
+
+            if ((data.config.trigger.type == "global") || (data.config.trigger.type == "flow"))
+            {
+                let ctx = RED.util.parseContextStore(data.config.trigger.value);
+                node.debug("[Timer:" + data.id + "] Load from context variable " + data.config.trigger.type + "." + ctx.key + (ctx.store ? " (" + ctx.store + ")" : ""));
+
+                let ctxData = node.context()[data.config.trigger.type].get(ctx.key, ctx.store);
+                if(!validateContextData(ctxData))
+                {
+                    node.error(RED._("scheduler.error.invalidEvent", {event: data.config.trigger.type + "." + ctx.key + (ctx.store ? " (" + ctx.store + ")" : "")}), {});
+                    return;
+                }
+
+                data.context = ctxData;
+            }
+
+            setUpTimer(data, false);
+        }
+
+        function stopTimer(data)
+        {
+            if ("timer" in data)
+            {
+                tearDownTimer(data);
+
+                if ("context" in data)
+                {
+                    delete data.context;
+                }
+            }
         }
 
         function setUpTimer(data, repeat)
         {
             try
             {
-                node.debug("Set up timer for type '" + data.trigger.type + "', value '" + data.trigger.value + (repeat ? "' (repeating)" : "'"));
+                node.debug("[Timer:" + data.id + "] Set up timer" + (repeat ? " (repeating)" : ""));
+
+                let event = ("context" in data) ? data.context : data.config;
+                node.trace("[Timer:" + data.id + "] Timer specification: " + JSON.stringify(event));
 
                 const now = chronos.getCurrentTime();
-                let triggerTime = chronos.getTime(repeat ? now.clone().add(1, "days") : now.clone(), data.trigger.type, data.trigger.value);
+                let triggerTime = chronos.getTime(repeat ? now.clone().add(1, "days") : now.clone(), event.trigger.type, event.trigger.value);
 
-                if (data.trigger.offset != 0)
+                if (event.trigger.offset != 0)
                 {
-                    let offset = data.trigger.random ? Math.round(Math.random() * data.trigger.offset) : data.trigger.offset;
+                    let offset = event.trigger.random ? Math.round(Math.random() * event.trigger.offset) : event.trigger.offset;
                     triggerTime.add(offset, "minutes");
                 }
 
                 if (triggerTime.isBefore(now))
                 {
-                    node.debug("Trigger time before current time, adding one day");
+                    node.debug("[Timer:" + data.id + "] Trigger time before current time, adding one day");
 
-                    if (data.trigger.type == "time")
+                    if (event.trigger.type == "time")
                     {
                         triggerTime.add(1, "days");
                     }
                     else
                     {
-                        triggerTime = chronos.getTime(triggerTime.add(1, "days"), data.trigger.type, data.trigger.value);
+                        triggerTime = chronos.getTime(triggerTime.add(1, "days"), event.trigger.type, event.trigger.value);
                     }
                 }
 
-                node.debug("Starting timer for trigger at " + triggerTime.format("YYYY-MM-DD HH:mm:ss"));
+                node.debug("[Timer:" + data.id + "] Starting timer for trigger at " + triggerTime.format("YYYY-MM-DD HH:mm:ss"));
                 data.timer = setTimeout(handleTimeout, triggerTime.diff(now), data);
             }
             catch (e)
@@ -307,7 +357,7 @@ module.exports = function(RED)
 
         function tearDownTimer(data)
         {
-            node.debug("Tear down timer for type '" + data.trigger.type + "', value '" + data.trigger.value + "'");
+            node.debug("[Timer:" + data.id + "] Tear down timer");
 
             clearTimeout(data.timer);
             delete data.timer;
@@ -317,28 +367,29 @@ module.exports = function(RED)
         {
             delete data.timer;
 
-            if ((data.output.type == "global") || (data.output.type == "flow"))
+            let event = ("context" in data) ? data.context : data.config;
+            if ((event.output.type == "global") || (event.output.type == "flow"))
             {
-                let ctx = RED.util.parseContextStore(data.output.property.name);
-                node.context()[data.output.type].set(ctx.key, data.output.property.value, ctx.store);
+                let ctx = RED.util.parseContextStore(event.output.property.name);
+                node.context()[event.output.type].set(ctx.key, event.output.property.value, ctx.store);
             }
-            else if (data.output.type == "msg")
+            else if (event.output.type == "msg")
             {
                 let msg = {};
-                if (data.output.property.type == "date")
+                if (event.output.property.type === "date")
                 {
-                    RED.util.setMessageProperty(msg, data.output.property.name, Date.now(), true);
+                    RED.util.setMessageProperty(msg, event.output.property.name, Date.now(), true);
                 }
                 else
                 {
-                    RED.util.setMessageProperty(msg, data.output.property.name, data.output.property.value, true);
+                    RED.util.setMessageProperty(msg, event.output.property.name, event.output.property.value, true);
                 }
 
                 sendMessage(data, msg);
             }
-            else if (data.output.type == "fullMsg")
+            else if (event.output.type == "fullMsg")
             {
-                sendMessage(data, data.output.value);
+                sendMessage(data, event.output.value);
             }
 
             setUpTimer(data, true);
@@ -348,14 +399,85 @@ module.exports = function(RED)
         {
             if (node.multiPort)
             {
-                node.ports[data.output.port] = msg;
+                node.ports[data.config.output.port] = msg;
                 node.send(node.ports);
-                node.ports[data.output.port] = null;
+                node.ports[data.config.output.port] = null;
             }
             else
             {
                 node.send(msg);
             }
+        }
+
+        function validateContextData(data)
+        {
+            if ((typeof data != "object") || !data)
+            {
+                return false;
+            }
+
+            if ((typeof data.trigger != "object") || !data.trigger)
+            {
+                return false;
+            }
+
+            if ((typeof data.trigger.type != "string") || !/^(time|sun|moon|custom)$/.test(data.trigger.type))
+            {
+                return false;
+            }
+
+            if ((typeof data.trigger.value != "string") ||
+                ((data.trigger.type == "time") && !chronos.isValidUserTime(data.trigger.value)) ||
+                ((data.trigger.type == "sun") && !/^(sunrise|sunriseEnd|sunsetStart|sunset|goldenHour|goldenHourEnd|night|nightEnd|dawn|nauticalDawn|dusk|nauticalDusk|solarNoon|nadir)$/.test(data.trigger.value)) ||
+                ((data.trigger.type == "moon") && !/^(rise|set)$/.test(data.trigger.value)))
+            {
+                return false;
+            }
+
+            if ((typeof data.trigger.offset != "number") || (data.trigger.offset < -300) || (data.trigger.offset > 300))
+            {
+                return false;
+            }
+
+            if (typeof data.trigger.random != "boolean")
+            {
+                return false;
+            }
+
+            if ((typeof data.output != "object") || !data.output)
+            {
+                return false;
+            }
+
+            if ((typeof data.output.type != "string") || !/^(global|flow|msg|fullMsg)$/.test(data.output.type))
+            {
+                return false;
+            }
+
+            if ((data.output.type == "fullMsg") && ((typeof data.output.value != "object") || !data.output.value))
+            {
+                return false;
+            }
+
+            if (data.output.type != "fullMsg")
+            {
+                if ((typeof data.output.property != "object") || !data.output.property)
+                {
+                    return false;
+                }
+
+                if ((typeof data.output.property.name != "string") || !data.output.property.name)
+                {
+                    return false;
+                }
+
+                if ((data.output.property.type !== "date") && (typeof data.output.property.value == "undefined"))
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
 
         function updateStatus()
